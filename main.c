27b9677673f05e07a2bf746f0b6916214e2ba9e4
Adelelsayedahmed <74651737+Adelelsayedahmed@users.noreply.github.com>
2023-03-04 05:00:27 +0200

    Added some comments

--- main.c
+++ main.c
@@ -16,33 +16,34 @@ int main()
   while (1)
   { 
     printf("Adel's shell is ready > ");
+	  
     take_input(&input_line);
-    set_number_of_args(&input_line);
-    if ( isThereArgs(&input_line) == 0 )
-          continue; ; 
+	  
+    set_number_of_args(&input_line);	
+	  
+    if ( 0 == isThereArgs(&input_line)  ) // check if the user press enter and the input line is empty or full of spaces 
+   	{ 
+	    continue; 
+    	}
     alloc_mem_for_arg(&input_line);
     parse_input(&input_line); 
-   
     pid_t pid = fork();
-    if (pid > 0) // executer process
+    if (pid > 0) // shell process
     {
       int wstatus;
-      waitpid(pid, &wstatus, 0);
-      dealloc_mem_for_arg(&input_line) ; 
+      waitpid(pid, &wstatus, 0);	// suspend the shell untill the new process terminated 
+      dealloc_mem_for_arg(&input_line) ; // deallocate the memory that is dynamically allocated to store the arguments
     }
 
     else if (pid == 0) // the new process
     {
-      char *const newargv[] = {NULL};
-      char *const newenvp[] = {"ADEL", NULL};
 
-
-	int flag = execvp(input_line.arg_list[0] ,(input_line.arg_list));
+	int flag = execvp(input_line.arg_list[0] ,(input_line.arg_list)); // load the image of the new program by passing the name of the executable and the argv[]
 
       if (-1 == flag)
       {
         printf("Failed to execute the program\n");
-        dealloc_mem_for_arg(&input_line) ; 
+        dealloc_mem_for_arg(&input_line) ; // deallocate the memory that is dynamically allocated to store the arguments in case of faliure to execute the new process
 
         return -1;
       }
